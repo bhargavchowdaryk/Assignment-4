2e056b14f3999889a8407e5569e6ee5b7761b99c
bhargavchowdaryk <104010340+bhargavchowdaryk@users.noreply.github.com>
2023-07-07 03:34:46 +0500

    Add files via upload

--- client/src/App.js
+++ client/src/App.js
@@ -1,1 +1,23 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Login from './components/Login';
+import Register from './components/Register';
+import './App.css';
 
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <Navbar />
+        <Switch>
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/register" component={Register} />
+          {/* Add other routes and components as needed */}
+        </Switch>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
